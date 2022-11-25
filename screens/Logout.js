f00252627ba231c9961622a49c424c6b2003a430
Bijoya <bijoya10101@gmail.com>
2022-11-25 12:30:47 +0530

    updates

--- screens/Logout.js
+++ screens/Logout.js
@@ -5,6 +5,7 @@ import firebase from "firebase";
 export default class Logout extends Component {
     componentDidMount() {
         firebase.auth().signOut();
+        this.props.navigation.replace("Login");
     }
     render() {
         return (
